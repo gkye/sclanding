67eb47fe8cc4badde7aa43db03cd0e80a7cc33a3
George Kye <iamkyegeorge@gmail.com>
2025-04-29 18:19:21 -0400

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
+const description = 'Plan, create, and record professional-quality video on your iPhone with ShutterCraft. Features timeline planning, stabilization, focus peaking, LUTs, and more.',
+
 export const metadata: Metadata = {
   title: 'Shutter Craft',
-  description: 'Shutter Craft. Record the same thing every time.',
+  description: description,
   generator: 'Shutter Craft Generator',
   icons: {
     icon: [
@@ -13,8 +15,23 @@ export const metadata: Metadata = {
     apple: [
       { url: '/sclanding/apple-icon.png' },
     ],
-  }
+  },
+   openGraph: {
+    title: 'ShutterCraft - Pro iPhone Video Recording',
+    description: description,
+    url: 'https://lauperlabs.com',
+    siteName: 'ShutterCraft',
+    locale: 'en_US',
+    type: 'website',
+  },
+  // Add Twitter card tags
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ShutterCraft - Pro iPhone Video Recording',
+    description: description,
+  },
 }
+
 
 export default function RootLayout({
   children,
