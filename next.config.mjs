57e59a6ad09d303aa2fa9140300fdd7cbc9a57af
George Kye <iamkyegeorge@gmail.com>
2025-04-29 15:40:09 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,8 @@ const nextConfig = {
 }
 if (!!process.env.PAGES_BASE_PATH && process.env.PAGES_BASE_PATH.length) {
 		console.warn('Assuming deployment to GitHub Pages', process.env.PAGES_BASE_PATH);
-		merge(nextConfig, {
-			output: 'export',
-			basePath: process.env.PAGES_BASE_PATH,
-		});
+    nextConfig.output = 'export';
+    nextConfig.basePath = process.env.PAGES_BASE_PATH;
 	} else {
     console.warn('Regular deployments deployment to GitHub Pages', process.env.PAGES_BASE_PATH);
   }
