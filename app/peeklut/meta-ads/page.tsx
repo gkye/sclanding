7ce0e44b34eb6eb6e55b22347947ef7998f8ab81
George Kye <iamkyegeorge@gmail.com>
2026-01-29 15:55:56 -0500

    Update page.tsx

--- app/peeklut/meta-ads/page.tsx
+++ app/peeklut/meta-ads/page.tsx
@@ -13,10 +13,10 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1198370492487431');
+fbq('init', '1228467055287081');
 fbq('track', 'PageView');`
 const metaPixelNoscript = `<img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1198370492487431&ev=PageView&noscript=1"/>`
+src="https://www.facebook.com/tr?id=1228467055287081&ev=PageView&noscript=1"/>`
 
 export const metadata: Metadata = {
   title: "PeekLUT – App Store Redirect",
